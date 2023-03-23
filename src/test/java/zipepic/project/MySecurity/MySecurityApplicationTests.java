09c0361e40955fea9199100ca1e1684a32590478
xzz1p <olezhka.kirilov.97@mail.ru>
2023-03-23 13:51:08 +0300

    Initial commit

---
+++ src/test/java/zipepic/project/MySecurity/MySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package zipepic.project.MySecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
